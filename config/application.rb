661c28d5bed87378604ab3bf3e08170ce217171f
Joe Sak <joe@joesak.com>
2018-11-12 09:09:48 -0600

    Set the server timezone to Mexico City

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,13 @@ module Dailybread
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
+    # Settings in config/environments/* take precedence over those specified here.c
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.time_zone = "Mexico City"
   end
 end
