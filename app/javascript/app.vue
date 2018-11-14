f0d5a00e74bbf6d06c3268e0749f454152a99525
Joe Sak <joe@joesak.com>
2018-11-14 10:33:25 -0600

    Add that famous Joe Sak footer

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -2,8 +2,8 @@
   <div id="app" class="container-fluid">
     <div class="row">
       <div class="col-8">
-        <h3>Dailybread</h3>
-        <h4>&#127838; Know Your Dough</h4>
+        <h3>&#127838; Dailybread</h3>
+        <h4>Know Your Dough</h4>
         <BillsList />
       </div>
 
@@ -12,6 +12,16 @@
         <BillForm />
       </div>
     </div>
+
+    <footer>
+      <p>
+        Dailybread was made with Ruby on Rails and Vue by Joseph M. Sak
+        <span><i>&#128247;</i><i>&#128248;</i></span>
+        <a href="https://github.com/joemsak/dailybread-2018/">
+          Contribute to the open source
+        </a>
+      </p>
+    </footer>
   </div>
 </template>
 
@@ -47,11 +57,60 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#app {
+  margin: 1rem 0;
+}
+
 .col-4 {
   position: fixed;
   z-index: 1;
   top: 0;
   right: 0;
   overflow-x: hidden;
+}
+
+footer {
+  margin: 1rem 0 0;
+  background: snow;
+  font-size: 0.9rem;
+  box-shadow: 0 -0.2rem 0.7rem rgba(0, 0, 0, 0.2);
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+}
+
+footer p {
+  padding: 0.5rem 1rem;
+  margin: 0;
+  font-size: 0.9rem;
+}
+
+footer span {
+  transition: transform 0.2s;
+  display: inline-block;
+  padding: 0 0.2rem;
+}
+
+footer span i {
+  font-style: normal;
+}
+
+footer span:hover {
+  transform: scale(1.5);
+  cursor: none;
+}
+
+footer span i:last-child {
+  display: none;
+}
+
+footer span:hover i:first-child {
+  display: none;
+}
+
+footer span:hover i:last-child {
+  display: inline-block;
 }
 </style>
