0848d06e040e0ec86861f0dfe21dcd13d42880dc
Joe Sak <joe@joesak.com>
2018-11-15 18:26:28 -0600

    Update contributor URL

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -9,7 +9,7 @@
         <p>
           Leftover Dough was made with Ruby on Rails and Vue by Joseph M. Sak
           <span><i>&#128247;</i><i>&#128248;</i></span>
-          <a href="https://github.com/joemsak/contribute-to-db2018/">
+          <a href="https://github.com/joemsak/contribute-to-leftover-dough">
             Earn a free year by contributing to the source
           </a>
         </p>
