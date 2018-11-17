f8ed193a0608361571f6431c4eb4064a3744cdb8
Joe Sak <joe@joesak.com>
2018-11-16 18:39:52 -0600

    Auto refresh the token when the window is active, every 8 minutes

--- app/javascript/packs/leftoverdough.js
+++ app/javascript/packs/leftoverdough.js
@@ -19,6 +19,44 @@ function initApp (el) {
   })
 }
 
+let refreshJWTIntervalId
+
+window.addEventListener('focus', autoRefreshJWT)
+
+function autoRefreshJWT () {
+  if (!refreshJWTIntervalId) {
+    const expireStr = window.localStorage.getItem('jwtExpiresAt')
+
+    const expiry = parseInt(expireStr) - 120 // minus 2 minutes
+    const now = Math.floor(new Date().getTime() / 1000)
+
+    const milliseconds = (expiry - now) * 1000
+
+    console.log(milliseconds)
+    if (milliseconds > 0)
+      refreshJWTIntervalId = setInterval(refreshJWT, milliseconds)
+  }
+}
+
+window.addEventListener('blur', () => {
+  clearInterval(refreshJWTIntervalId)
+  refreshJWTIntervalId = 0
+})
+
+function refreshJWT () {
+  const refreshToken = window.localStorage.getItem('refreshToken')
+  Api.post('/access_token_refreshes', { token: refreshToken })
+    .then(json => setJWT(json))
+}
+
+function setJWT (json) {
+  window.localStorage.setItem('jwt', json.jwt)
+  window.localStorage.setItem('jwtExpiresAt', json.expiresAt)
+  window.localStorage.setItem('refreshToken', json.refreshToken)
+  clearInterval(refreshJWTIntervalId)
+  autoRefreshJWT()
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const el = document.body.querySelector("#leftoverdough-app")
   const search = window.location.search
@@ -26,34 +64,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (window.location.pathname.match(/signin|signup/)) {
     return false
+
   } else if (window.location.pathname.match(/logout/)) {
     window.localStorage.removeItem('jwt')
     window.location.href = '/signin'
     return false
+
   } else if (search && search.match(/magicLinkToken=/)) {
     const token = search.match(/magicLinkToken=(\w+)/)[1]
 
     Api.post('/access_tokens', { token })
       .then(json => {
-        window.localStorage.setItem('jwt', json.jwt)
+        setJWT(json)
         window.history.replaceState(null, null, window.location.pathname)
         initApp(el)
         return false
       })
+
   } else if (search && search.match(/emailConfirmationToken=/)) {
     const token = search.match(/emailConfirmationToken=(\w+)/)[1]
 
     Api.post('/email_confirmations', { token })
       .then(json => {
-        window.localStorage.setItem('jwt', json.jwt)
+        setJWT(json)
         window.history.replaceState(null, null, window.location.pathname)
         initApp(el)
         return false
       })
+
   } else if (el && jwt) {
     initApp(el)
     return false
+
   } else {
     window.location.href = '/signin'
+
   }
 })
