90a231d6c9a795fae074b4566960f591ddbf6f81
Joe Sak <joe@joesak.com>
2018-11-16 19:07:40 -0600

    Don't duplicate user emails on create

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,7 +1,12 @@
 class V1::UsersController < ApplicationController
   def create
-    user = V1::User.create!(user_params)
-    SignupMailer.send_confirmation_email(user).deliver_later
+    user = V1::User.find_or_create_by!(user_params)
+
+    if user.pending?
+      SignupMailer.send_confirmation_email(user).deliver_later
+    else
+      SigninMailer.send_magic_link(user).deliver_later
+    end
   end
 
   private
--- spec/requests/v1/signup_requests_spec.rb
+++ spec/requests/v1/signup_requests_spec.rb
@@ -28,5 +28,42 @@ RSpec.describe "Signups" do
         SignupMailer.send_confirmation_email.deliver_later
       }.to have_enqueued_job.on_queue('mailers')
     end
+
+    it "sends an existing pending user another email" do
+      user = FactoryBot.create(:user, email: "joe@joesak.com")
+
+      allow(SignupMailer).to receive(:send_confirmation_email).and_call_original
+
+      expect {
+        post v1_users_path, params: {
+          email: "joe@joesak.com"
+        }
+      }.not_to change {
+        V1::User.pending.count
+      }.from(1)
+
+      expect(SignupMailer).to have_received(:send_confirmation_email).with(user)
+
+      expect {
+        SignupMailer.send_confirmation_email.deliver_later
+      }.to have_enqueued_job.on_queue('mailers')
+    end
+
+    it "sends an existing confirmed user a signin email" do
+      user = FactoryBot.create(:user, :confirmed, email: "joe@joesak.com")
+
+      allow(SigninMailer).to receive(:send_magic_link).and_call_original
+      expect(SignupMailer).not_to receive(:send_confirmation_email)
+
+      post v1_users_path, params: {
+        email: "joe@joesak.com"
+      }
+
+      expect(SigninMailer).to have_received(:send_magic_link).with(user)
+
+      expect {
+        SigninMailer.send_magic_link.deliver_later
+      }.to have_enqueued_job.on_queue('mailers')
+    end
   end
 end
